9027bdf3063708a14237c4d768fcb8261aa5b906
Roberto Antonio Berrospe Machin <roberto@Robertos-MacBook-Pro.local>
2017-12-27 01:06:29 -0300

    Nueva Versión

---
+++ README.md
@@ -0,0 +1,22 @@
+# TwitterAnalyticsPythonScript
+Twitter text mining de menciones y palabras mas usadas.
+
+Por ahora solo genera dos nubes de palabras (wordcloud) con los usuarios mas activos, de las palabras, y hashtags mas relevantes.
+
+## Ejemplo de resultados para "@Uruguay OR #Uruguay"
+
+### Usuarios más activos (screen_names) 
+
+![alt text](tda_mostactive.png "Usuarios mas activos")
+
+
+### Palabras mas relevantes
+
+![alt text](tda_words.png "Palabras mas relevantes")
+
+
+### Hashtags mas relevantes
+![alt text](tda_hashtags.png "Hashtags mas relevantes")
+
+
+Nota: Este script es inspirado en la versión para R por [@d4tagirl](https://github.com/d4tagirl/DuraznoConfMentions), ¡una genia! ¡gracias!
---
+++ visualizaciones.py
@@ -0,0 +1,244 @@
+#----------------------------------------------------------------------------
+# Librerías
+#----------------------------------------------------------------------------
+from twython import Twython
+from wordcloud import WordCloud, STOPWORDS
+from pandas import DataFrame
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+import sys
+
+#----------------------------------------------------------------------------
+# Variables Globales
+#----------------------------------------------------------------------------
+usandoLineaDeComandos = False
+
+#----------------------------------------------------------------------------
+# Funciones
+#----------------------------------------------------------------------------
+
+# Creacion de WordCloud
+# La salida es en el directorio actual
+def generateWordCloud(words, width, height, bg_color, words_count, output_file_name):
+    stopwords = set(STOPWORDS)
+    text = ' '.join(words)
+    wc = WordCloud(width=width,
+                   height=height,
+                   background_color=bg_color,
+                   max_words=words_count,
+                   stopwords=stopwords,
+                   random_state=1,
+                   collocations=False).generate(text)
+    # Si se usa línea de comandos, se requiere todo el path de salida
+    if usandoLineaDeComandos is True:
+        wc.to_file(output_file_name)
+    # De lo contrario se pone en el directorio actual/images
+    else:
+        wc.to_file(os.path.join(os.getcwd(), output_file_name))
+
+
+# Funcion que toma todo el texto, lo limpia
+# de menciones, hastags, emoticones, URL's, símbolos,
+# y palabras comunes no importantes para el cometido
+# retorna un list con todas las palabras
+def cleanAndTokenizeText(list_TwitsText):
+    # patter para eliminar emoticons
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+
+    # cargo todo el texto en un solo string
+    clean_text = ' '.join(list_TwitsText)
+    # eliminar menciones
+    clean_text = re.sub(r'@([a-zA-Z0-9\\_\\.]+)', '', clean_text)
+    # eliminar urls
+    clean_text = re.sub(r'https:([a-zA-Z0-9\\_\\.\\/]+)', '', clean_text)
+    # eliminar hashtags
+    clean_text = re.sub('#([a-zA-Z0-9\\_\\.]+)', '', clean_text)
+    # eliminar números
+    clean_text = re.sub(r'[0-9]+', '', clean_text)
+    # eliminar emojis
+    clean_text = emoji_pattern.sub('', clean_text)
+    # eliminar otros símbolos
+    clean_text = re.sub(r'[^\w\ \_]', '', clean_text)
+    # tokenizar palabras nuevamente
+    terms_all = clean_text.split()
+    # filtro/exluyo palabras no requeridas
+    filtered_words = [
+        word
+        for word in terms_all
+        if word not in stopwords.words('english')
+        and
+        word not in stopwords.words('spanish')
+        and
+        len(word) > 3  # prevenir la, el, acá, y cosas por el estilo
+    ]
+    # retorno las palabras
+    return filtered_words
+
+
+#----------------------------------------------------------------------------
+# Inicialización de Twitter
+# Debes crear una aplicacion en https://apps.twitter.com para obtener
+# el APP_KEY y el APP_SECRET
+#----------------------------------------------------------------------------
+# Authorizo Twython
+APP_KEY = ''
+APP_SECRET = ''
+twythonInst = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twythonInst.obtain_access_token()
+twitterClient = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+#----------------------------------------------------------------------------
+# Bajo últimos 100 tweets excluyendo los retweets
+#------------------------------------------------------------------------------
+
+# Para permitir usar en línea de comandos, y pasarle lo que se desea buscar
+# si se paso los keywords via línea de commandos
+try:
+    query = sys.argv[1]
+    usandoLineaDeComandos = True
+except:  # si no, cargar predeterminado
+    query = "@Uruguay OR #Uruguay"
+
+# Buscar...
+queryResult = twitterClient.search(q=query + ' -filter:retweets',
+                              count=100)
+
+# Si no se encontraron twits, salir
+if len(queryResult['statuses']) == 0:
+    print("No twits found for the specified search keywords")
+    sys.exit(1)
+
+#----------------------------------------------------------------------------
+# Proceso los nombres de los archivos de salida en la línea de comandos
+#----------------------------------------------------------------------------
+
+if usandoLineaDeComandos == True:
+    try:  # intento cargar los nombres
+        # nombre de archivo para screen names (usuarios mas activos)
+        mostactiveusers_file_name = sys.argv[2]
+        # nombre de archivo para palabras mas usadas
+        mostusedwords_file_name = sys.argv[3]
+        # nombre de archivo para nube de hashtags
+        hashtags_file_name = sys.argv[4]
+    except:  # si alguno de los nombres no fue pasado error y salir
+        print("\n\nPlease specify desired output images filenames. Ex: \"tda_mentions\" \"tda_words\"")
+        sys.exit(1)
+else:  # en caso de no ser línea de commandos, asumir nombres
+    mostactiveusers_file_name = 'tda_mostactive.png'
+    mostusedwords_file_name = 'tda_words.png'
+    hashtags_file_name = 'tda_hashtags.png'
+
+
+#----------------------------------------------------------------------------
+# Descargar stopwords ...
+#----------------------------------------------------------------------------
+nltk.download("stopwords", quiet=True)
+
+
+
+#----------------------------------------------------------------------------
+# Conversion a DataFrame para mejor manipulacion
+# Extracción de URL's, HashTags, screen_names, y demás info
+#----------------------------------------------------------------------------
+#creo dataframe vacío con las columnas a usar
+df_tweets = DataFrame(columns=['ids','texts','screen_names','names','locations','descriptions','langs','hashtags','urls'])
+#creao una lista vacía que contendrá los hashtags
+lst_hashtags = []
+#creo una lista vacía que contendrá las URL's 
+lst_urls = []
+#recorro todos los statuses
+for status in queryResult['statuses']:
+    #lista hashtag vacía
+    hashtags=[]
+    #si hay alguna hashtag
+    if len(status['entities']['hashtags']) > 0:
+        #recorro todos los hashtags y obtengo el texto en minúsculas
+        hashtags = [hashtag['text'].lower() for hashtag in status['entities']['hashtags']]
+        #lo agrego a la lista de hashtags global
+        lst_hashtags.extend(hashtags)
+    #lista urls vacía
+    urls=[]
+    #si hay alguna URL
+    if len(status['entities']['urls']) > 0:
+        #recorro todas las urls, extraigo la url expandida y la agrego a urls
+        urls = [url['expanded_url'] for url in status['entities']['urls']]
+        #agrego las urls encontradas a la lista
+        lst_urls.extend(urls)
+    #agrego nueva fila de datos al dataframe
+    df_tweets = df_tweets.append({
+            'ids': status['id'],
+            'texts': status['text'],
+            'screen_names': status['user']['screen_name'],
+            'names': status['user']['name'],
+            'locations': status['user']['location'],
+            'descriptions': status['user']['description'],
+            'langs': status['user']['lang'],
+            'hashtags':hashtags,
+            'urls':urls
+            },ignore_index=True)
+
+
+#----------------------------------------------------------------------------
+# Extracción de Screen Names
+#----------------------------------------------------------------------------
+
+
+# Hago una nube de palabras de los usuarios que mas tuitearon sobre el tema
+# y con un máximo de 60 screen names
+generateWordCloud(words=df_tweets['screen_names'],
+                  width=800,
+                  height=400,
+                  bg_color='black',
+                  words_count=60,
+                  output_file_name=mostactiveusers_file_name)
+
+#----------------------------------------------------------------------------
+# Extracción de Palabras Mas Usadas
+#----------------------------------------------------------------------------
+
+# Veo qué palabras son las más usadas en los tueets que mencionan los keywords,
+# sacando los emoticones, las menciones, las urls, y los hastags
+tweets_words = cleanAndTokenizeText(df_tweets['texts'])
+
+# Hago una nube de palabras con las palabras mas usadas
+# y con un máximo de 60 palabras mostradas
+generateWordCloud(words=tweets_words,
+                  width=800,
+                  height=400,
+                  bg_color='black',
+                  words_count=60,
+                  output_file_name=mostusedwords_file_name)
+
+
+#----------------------------------------------------------------------------
+# Extracción de HashTags mas usados
+#----------------------------------------------------------------------------
+
+
+# Hago una nube de palabras con los hashtags mas usados
+# y con un máximo de 60 palabras mostradas
+generateWordCloud(words=lst_hashtags,
+                  width=800,
+                  height=400,
+                  bg_color='black',
+                  words_count=60,
+                  output_file_name=hashtags_file_name)
+
+
+# Si estamos en línea de comandos, devolver los caminos a los archivos generados
+if usandoLineaDeComandos == True:
+    print(mostactiveusers_file_name)
+    print(mostusedwords_file_name)
+    print(hashtags_file_name)
+else:
+    print(os.path.join(os.getcwd(), mostactiveusers_file_name))
+    print(os.path.join(os.getcwd(), mostusedwords_file_name))
+    print(os.path.join(os.getcwd(), hashtags_file_name))
+    print("Done!")
